2b0cd46155898c2ced84aec6693abb05dbdb6e60
kharioki <tony.kharioki@gmail.com>
2019-02-28 17:12:20 +0300

    added redux

---
+++ src/components/Hello.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+
+import './Hello.css';
+
+//TODO: write tests for onIncrement and onDecrement
+
+export interface Props {
+  name: String;
+  enthusiasmLevel?: number;
+  onIncrement?: () => void;
+  onDecrement?: () => void;
+}
+
+function Hello({ name, enthusiasmLevel = 1, onIncrement, onDecrement }: Props) {
+  if (enthusiasmLevel <= 0) {
+    throw new Error('You could be a little more enthusiastic. :D');
+  }
+
+  return (
+    <div className="hello">
+      <div className="greeting">
+        Hello {name + getExclamationMarks(enthusiasmLevel)}
+      </div>
+      <div>
+        <button onClick={onDecrement}>-</button>
+        <button onClick={onIncrement}>+</button>
+      </div>
+    </div>
+  );
+}
+
+export default Hello;
+
+// helpers
+
+function getExclamationMarks(numChars: number) {
+  return Array(numChars + 1).join('!');
+}
